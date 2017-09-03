c8170fbc672aa2d0de195dd4a4a232464e864535
Salma El Alaoui Talibi <s.elalaouitalibi@gmail.com>
2017-09-03 23:01:33 +0900

    add tasks for pp=f(%documents) and pp=f(nb_topics)

--- src/variational_bayes_ctm/predictive_perplexity_ctm.py
+++ src/variational_bayes_ctm/predictive_perplexity_ctm.py
@@ -31,22 +31,27 @@ def perplexity_ctm(train_sizes, dataset, number_topics, save=False, output_direc
 
 
 def perplexity_topics(topic_numbers, train_size, dataset, output_directory):
-    print("-----------------CTM-----------------------")
     print("Train size ", train_size)
+    perplexities_tr = list()
+    perplexities_te = list()
     for t in topic_numbers:
         print("===============Number of topics==============", t)
-        perplexities_tr, perplexities_te = perplexity_ctm(train_sizes=[train_size], dataset=dataset, number_topics=t)
-        print("Train CTM: ", perplexities_tr)
-        print("Test CTM: ", perplexities_te)
-        np.savetxt(os.path.join(output_directory, str(train_size) + '_log_perplexities_ctm_topics.txt'),
-                   (topic_numbers, np.asarray(perplexities_tr), np.asarray(perplexities_te)))
+        tr, te = perplexity_ctm(train_sizes=[train_size], dataset=dataset, number_topics=t)
+        perplexities_tr.append(tr[0])
+        perplexities_te.append(te[0])
+
+    print("Train CTM: ", perplexities_tr)
+    print("Test CTM: ", perplexities_te)
+    np.savetxt(os.path.join(output_directory, str(train_size) + '_log_perplexities_ctm_topics.txt'),
+               (topic_numbers, np.asarray(perplexities_tr), np.asarray(perplexities_te)))
 
 
 if __name__ == "__main__":
     out = "../../results/20_news_groups"
     d = NewsDataset
     k = 10
-    perplexity_ctm(train_sizes=np.linspace(0.1, 0.9, num=9), dataset=d, number_topics=k, save=True, output_directory=out)
+    train = np.linspace(0.1, 0.9, num=9)
+    perplexity_ctm(train_sizes=train, dataset=d, number_topics=k, save=True, output_directory=out)
 
 
 
