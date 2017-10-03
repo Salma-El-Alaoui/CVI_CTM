90cbdde6b67a97a76e528b1abb322dd55f49bbd5
Salma El Alaoui Talibi <s.elalaouitalibi@gmail.com>
2017-10-03 12:37:30 +0900

    clean script to test CVI

--- src/experiments/cvi_test.py
+++ src/experiments/cvi_test.py
@@ -1,19 +1,22 @@
-from src.variational_bayes_ctm.corpus import ToyDataset, NewsDataset, ApDataset
+from src.variational_bayes_ctm.corpus import NewsDataset, DeNewsDataset
 from src.variational_bayes_ctm.ctm_cvi import CTM_CVI
 from src.variational_bayes_ctm.ctm import CTM
 import numpy as np
 import os
 
 train_size = 0.7
-data = NewsDataset(train_size=train_size)
-output_directory = "../../results/20_news_groups"
+data = DeNewsDataset(train_size=train_size)
+output_directory = "../../results/de-news"
 
-number_topics = 20
-em_iter = 50
+number_topics = 10
+
+em_iter = 30
 e_iter = 30
 
 cvi = True
 step_size = 0.7
+
+print("Dataset: ", str(data))
 
 if cvi:
     filename = 'conv_cvi_S=%d.txt' % (step_size * 10)
@@ -21,7 +24,7 @@ if cvi:
     ctm = CTM_CVI(corpus=data.doc_set_train, vocab=data.vocabulary, number_of_topics=number_topics, em_max_iter=em_iter,
                   step_size=step_size, local_param_iter=e_iter)
 else:
-    filename = 'conv_vb'
+    filename = 'conv_vb.txt'
     print("======================CTM-VB==================")
     ctm = CTM(corpus=data.doc_set_train, vocab=data.vocabulary, number_of_topics=number_topics, em_max_iter=em_iter,
               local_param_iter=e_iter)
