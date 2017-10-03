3e5ed0238a7dbca94982a0a71a021f3494c97b37
Salma El Alaoui Talibi <s.elalaouitalibi@gmail.com>
2017-10-03 12:36:06 +0900

    clean script for testing stochastic CVI

--- src/experiments/stochastic_cvi_test.py
+++ src/experiments/stochastic_cvi_test.py
@@ -1,23 +1,24 @@
 import os
 import numpy as np
-from src.variational_bayes_ctm.corpus import NewsDataset
+from src.variational_bayes_ctm.corpus import NewsDataset, DeNewsDataset
 from src.variational_bayes_ctm.ctm_cvi_stochastic import CTM_CVI_S
 
 train_size = 0.7
 data = NewsDataset(train_size=train_size)
-
 number_topics = 20
-em_iter = 50
+
+em_iter = 100
 e_iter = 30
 
+batch_size = 150
+learning_offset = 10
+learning_decay = 0.7
 step_size = 0.7
-batch_size = 300
-learning_offset = 64
-learning_decay = 0.6
-evaluate_every = 10
+evaluate_every = 5
+
 output_directory = "../../results/20_news_groups"
-filename = 'conv_stoch_cvi_B=%d_OF=%d_D=%d_S=%d.txt' % (
-batch_size, learning_offset, learning_decay * 10, step_size * 10)
+filename = 'conv_stoch_cvi_B=%d_OF=%d_D=%d_S=%d_every5.txt' % (batch_size,
+                                                               learning_offset, learning_decay * 10, step_size * 10)
 
 print("=====================STOCHASTIC CVI=====================")
 ctm = CTM_CVI_S(corpus=data.doc_set_train, vocab=data.vocabulary, number_of_topics=number_topics, em_max_iter=em_iter,
